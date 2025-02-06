f8a9b459ad3890e2b18293ef7cb8d8052a3d2ba6
Dimitar Shtregarski <mite_sht@abv.bg>
2025-02-06 13:37:54 +0200

    New changes

---
+++ Grade_10/Term_01/Week_19_Structs_3_03_02_2025/Solutions/clock.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+struct clock
+{
+    int hours;
+    int mins;
+    int seconds;
+
+    /// Constructor - създава обект от тип clock
+    clock(int newHours, int newMins, int newSeconds)
+    {
+
+    }
+
+};
+
+
+
+int main()
+{
+
+
+
+    return 0;
+}
